9bbd72022204ae9783f8e133b53e19d90b7fb00a
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-21 17:44:59 +0200

    Spring Framework -  Publisher Relationships l.18

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -42,8 +42,12 @@ public class BootStrapData implements CommandLineRunner {
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J@EE Developer without EJB", "2311233");
@@ -51,11 +55,16 @@ public class BootStrapData implements CommandLineRunner {
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
 
 
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher nubmer of books: " + publisher.getBooks().size());
     }
 }
